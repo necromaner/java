5f2610ecc818bd468754e1c87155018f331b7dbc
necromaner <necromaner@foxmail.com>
2017-10-09 23:16:10 +0800

    10.9

--- LeetCode/src/Leetcode_11_10_RegularExpressionMatching.java
+++ LeetCode/src/Leetcode_11_10_RegularExpressionMatching.java
@@ -5,6 +5,26 @@
  * Date: 2017-10-09
  * Time: 下午11:10
  */
+/*
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
+ */
 public class Leetcode_11_10_RegularExpressionMatching {
     public static void main(String[] args) {
         Solution solution = new Solution();
