ce786b08615c0770cf042c2b51025015954e68bf
necromaner <necromaner@foxmail.com>
2017-12-27 16:04:40 +0800

    12.27

--- learn/src/String/String1.java
+++ learn/src/String/String1.java
@@ -8,8 +8,20 @@ package String;
  */
 public class String1 {
     public static void main(String[] args) {
-        char[] x={'a','b','c','c','e','s','d','c'};
-        String x2= new String(x,1,5);
-        System.out.println(x2);
+//        char[] x={'a','b','c','c','e','s','d','c'};
+//        String x2= new String(x,1,5);
+//        System.out.println(x2);
+        String s="qewegh wegretnh";
+        System.out.println(initcap(s));
+        
+    }
+    public static String initcap(String str){
+        if (str==null&&"".equals(str)){
+            return str;
+        }
+         if (str.length()>1){
+            return str.substring(0,1).toUpperCase()+str.substring(1);
+         }
+         return str.toUpperCase();
     }
 }
