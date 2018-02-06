42b28449021df1e9fb814dd4dbf9a0ea3accb8cf
necromaner <necromaner@foxmail.com>
2018-02-06 21:08:45 +0800

    2.6

--- ChatRoom/src/Client.java
+++ ChatRoom/src/Client.java
@@ -1,5 +1,7 @@
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 import static java.net.InetAddress.getLocalHost;
 
@@ -10,10 +12,23 @@ public class Client {
     }
     public Client() {
         try {
-            socket=new Socket(InetAddress.getLocalHost(),5000);
+            Scanner input = new Scanner(System.in);
+//            System.out.print("输入连接ip:");
+//            String ip=input.nextLine();
+//            System.out.print("输入连接端口号:");
+//            String port=input.nextLine();
+            socket=new Socket(InetAddress.getLocalHost().getHostAddress(),5000);
             System.out.println("连接成功");
+            System.out.println(socket);
         }catch (Exception e){
             System.out.println("连接失败");
+        }
+        try {
+            socket.close();
+            System.out.println("关闭成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("关闭失败");
         }
     }
 }
--- ChatRoom/src/Server.java
+++ ChatRoom/src/Server.java
@@ -1,23 +1,36 @@
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    
     private ServerSocket serverSocket;
     private Socket socket;
     public static void main(String[] args)  {
         new Server();
     }
     public Server() {
+        
         try {
+            String addr = InetAddress.getLocalHost().getHostAddress();//获得本机IP
+            System.out.println("服务器IP："+addr);
             serverSocket =new ServerSocket(5000);
             System.out.println("启动成功");
-            socket=serverSocket.accept();
-            
+            while (true) {
+                socket = serverSocket.accept();
+                System.out.println(socket);
+            }
         }
         catch (Exception e){
             System.out.println("启动失败");
         }
-        
+        try{
+            serverSocket.close();
+            socket.close();
+            System.out.println("关闭成功");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("关闭失败");
+        }
     }
 }
