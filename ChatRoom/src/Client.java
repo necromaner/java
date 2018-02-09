907e743ea10646a4050152fe4aa77c766940f164
necromaner <necromaner@foxmail.com>
2018-02-10 00:47:39 +0800

    2.9

--- ChatRoom/src/Client.java
+++ ChatRoom/src/Client.java
@@ -16,6 +16,7 @@ public class Client {
     private BufferedReader br = null;
     private boolean close = false;
     static Scanner scanner = new Scanner(System.in);
+    private String name="";
     
     public static void main(String[] args) {
         new Client();
@@ -48,34 +49,23 @@ public class Client {
             socket = new Socket(InetAddress.getLocalHost().getHostAddress(), 5000);
             System.out.println("连接成功");
             System.out.println(socket);
+            System.out.print("请输入昵称： ");
+            name=input.nextLine();
+            if ("".equals(name)){
+                name=InetAddress.getLocalHost().getAddress().toString();
+            }
             br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            new test2(socket).start();
-            new test1(socket).start();
+            new OutPut(socket).start();
+            new InPut(socket).start();
         } catch (Exception e) {
+            System.out.println("+++++++++++1++++++++++++");
             System.out.println("连接失败");
         }
     }
-    
-    public void Start() {
-        try {
-            Scanner input = new Scanner(System.in);
-//            System.out.print("输入连接ip:");
-//            String ip=input.nextLine();
-//            System.out.print("输入连接端口号:");
-//            String port=input.nextLine();
-            socket = new Socket(InetAddress.getLocalHost().getHostAddress(), 5000);
-            System.out.println("连接成功");
-            System.out.println(socket);
-        } catch (Exception e) {
-            System.out.println("连接失败");
-        }
-    }
-    
-
-    public class test1 extends Thread{
+    public class InPut extends Thread{
         Socket socket1;
-        public test1(Socket socket){
+        public InPut(Socket socket){
             this.socket1=socket;
         }
         public void run() {
@@ -84,34 +74,60 @@ public class Client {
                     
 //                    System.out.println("Client端请输入：");
                     String str = scanner.next();
-                    pw.println(str);
+                    if ("======ERROR======".equals(Agreement(str))){
+                        break;
+                    }
+                    pw.println(Agreement(str));
                     pw.flush();
                     System.out.println("test--1");
                 }
             }catch (Exception e) {
+                System.out.println("+++++++++++2++++++++++++");
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
+        /**
+        协议:
+         ==exit==     关闭客户端
+         */
+        public String Agreement(String s){
+            if ("==exit==".equals(s)) {
+                Close();
+                return "======ERROR======";
+            }else {
+            String[] s1 = s.split("=-=",2);
+            System.out.println("----1----"+s1.length);
+            if (s1.length==2) {
+                String[] s2 = s1[0].split("\\.");
+                System.out.println("----2----"+s2.length);
+                System.out.println();
+                if (s2.length == 4) {
+                    String[] s3 = s2[3].split(":");
+                    System.out.println("----3----"+s3.length);
+                    if (s3.length == 2)
+                        return name + "|-=-=-=-|/" + s1[0] + "|-=-=-=-|" + s1[1];
+                }
+            }
+            
+            
+            return name+"|-=-=-=-|"+"ALL"+"|-=-=-=-|"+s;
+            }
+        }
     }
-    public class test2 extends Thread{
+    public class OutPut extends Thread{
         Socket socket1;
-        public test2(Socket socket){
+        public OutPut(Socket socket){
             this.socket1=socket;
         }
         public void run() {
             try {
                 while(true){
-//                    System.out.println("Client端请输入：");
-//                    String str = scanner.next();
-//                    pw.println(str);
-//                    pw.flush();
                     String string=br.readLine();
                     System.out.println("Client读到："+string);
-                    System.out.println("test--2");
-//                    System.out.println(string);
                 }
             }catch (Exception e) {
+                System.out.println("+++++++++++3++++++++++++");
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
@@ -121,8 +137,10 @@ public class Client {
         try {
             close = true;
             socket.close();
+            System.exit(0);
             System.out.println("关闭成功");
         } catch (IOException e) {
+            System.out.println("+++++++++++4++++++++++++");
             System.out.println("关闭失败");
             e.printStackTrace();
         }
--- ChatRoom/src/Server.java
+++ ChatRoom/src/Server.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 
 public class Server {
     List<ServerThread> clients = new ArrayList<ServerThread>();
+    
     public static void main(String[] args) {
         new Server();
     }
@@ -21,15 +22,17 @@ public class Server {
             System.out.println("启动成功");
             while (true) {
                 Socket socket = serverSocket.accept();
-                ServerThread serverThread=new ServerThread(socket);
+                ServerThread serverThread = new ServerThread(socket);
                 serverThread.start();
                 clients.add(serverThread);
-                System.out.println("连接人数： "+clients.size());
+                System.out.println("连接人数： " + clients.size());
                 
             }
         } catch (UnknownHostException e) {
+            System.out.println("+++++++++++1++++++++++++");
             e.printStackTrace();
         } catch (IOException e) {
+            System.out.println("+++++++++++2++++++++++++");
             System.out.println("启动失败");
             e.printStackTrace();
         }
@@ -49,8 +52,10 @@ public class Server {
                 reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 //            System.out.println(socket);
 //            System.out.println(socket.getRemoteSocketAddress());
-                System.out.println("addr：" + socket.getInetAddress() + " ；port: " + socket.getPort() + " 已连接");
+//                System.out.println("addr：" + socket.getInetAddress() + " ；port: " + socket.getPort() + " 已连接");
+                System.out.println(socket.getRemoteSocketAddress() + " 已连接");
             } catch (IOException e) {
+                System.out.println("+++++++++++3++++++++++++");
                 System.out.println("客户端连接失败");
                 e.printStackTrace();
             }
@@ -67,154 +72,111 @@ public class Server {
                     break;
             }
         }
-
+    
         public void InPut() {
             try {
                 reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String string = reader.readLine();
-                System.out.println("从 "+socket.getRemoteSocketAddress() + " 发来消息 ：" + string);
+                System.out.println("-|从 " + socket.getRemoteSocketAddress() + " 发来消息 ：" + string);
 //                OutPut(string);
-                for (int i = 0; i < clients.size(); i++) {
-                    if (socket!=clients.get(i).socket) {
-                        System.out.println("给 " + clients.get(i).socket.getRemoteSocketAddress() + " 发送消息： " + string);
-                        ServerThread thread = clients.get(i);
-                        thread.OutPut(string);
-                    }
-                }
+                String[] ss1 = string.split("\\|-=-=-=-\\|");
+                System.out.println("昵称： " + ss1[0]);
+                System.out.println("发送给： " + ss1[1]);
+                System.out.println("发送消息： " + ss1[2]);
+    
+                if ("ALL".equals(ss1[1])) {
+                    SendALL(ss1);
+                } else
+                    SendAppoint(ss1);
                 System.out.println("—————————————————————————————————————————");
             } catch (IOException e) {
+                System.out.println("+++++++++++4++++++++++++");
                 e.printStackTrace();
+            } catch (NullPointerException e) {
+                System.out.println("+++++++++++5++++++++++++");
+                Close();
             }
         }
-
+    
+        public void SendALL(String[] string) {
+            System.out.println("--------群发--------发送人数： "+(clients.size()-1));
+            for (int i = 0; i < clients.size(); i++) {
+                if (socket != clients.get(i).socket) {
+                    System.out.println("    -|给 " + clients.get(i).socket.getRemoteSocketAddress() + " 发送消息： " + string[0] + "||=-=-=-=||" + string[2]);
+                    ServerThread thread = clients.get(i);
+                    thread.OutPut(string[0] + "(" + clients.get(i).socket.getRemoteSocketAddress() + ") :" + string[2]);
+                }
+            }
+        }
+    
+        public void SendAppoint(String[] string) {
+            boolean b = true;
+            System.out.println("--------指定发--------");
+            String[] s1 = string[1].split("<-=-=-=->");
+            System.out.println("发送给" + s1.length + "个人");
+            System.out.println("Address :");
+            for (int i = 0; i < s1.length; i++) {
+                System.out.println(i + ". " + s1[i]);
+            }
+            for (int i = 0; i < clients.size(); i++) {
+                System.out.println("-|" + i + ". " + clients.get(i).socket.getRemoteSocketAddress().toString());
+                for (int j = 0; j < s1.length; j++) {
+                    System.out.println("    -|" + j + ". " + s1[j]);
+                    if (s1[j].equals(clients.get(i).socket.getRemoteSocketAddress().toString())) {
+                        b = false;
+                        System.out.println("    -|给 " + clients.get(i).socket.getRemoteSocketAddress() + " 发送消息： " + string[0] + "||=-=-=-=||" + string[2]);
+                        ServerThread thread = clients.get(i);
+                        thread.OutPut(string[0] + "(" + clients.get(i).socket.getRemoteSocketAddress() + ") :" + string[2]);
+                    }
+                }
+            }
+            if (b) {
+                System.out.println("失败");
+                for (int i = 0; i < clients.size(); i++) {
+                    if (socket == clients.get(i).socket) {
+                        ServerThread thread = clients.get(i);
+                        thread.OutPut("发送给(" + clients.get(i).socket.getRemoteSocketAddress() + ")发送失败");
+                    }
+                }
+            }
+        }
+    
         public void OutPut(String string) {
             try {
                 writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
 //            String str = scanner.next();
-
+    
                 writer.println(string);
                 writer.flush();
                 if (string.equals("exit")) {
                     Close();
                 }
             } catch (IOException e) {
+                System.out.println("+++++++++++6++++++++++++");
                 e.printStackTrace();
             }
         }
-
-
+    
+    
         public void Close() {
             try {
-//            serverSocket.close();
                 socket.close();
                 writer.close();
                 reader.close();
+                for (int i = 0; i < clients.size(); i++) {
+                    if (clients.get(i).socket == socket) {
+                        clients.remove(i);
+                        break;
+                    }
+                }
                 System.out.println("addr：" + socket.getInetAddress() + " ；port: " + socket.getPort() + " 已退出");
+                System.out.println("连接人数： " + clients.size());
             } catch (IOException e) {
+                System.out.println("+++++++++++7++++++++++++");
                 e.printStackTrace();
                 System.out.println("关闭失败");
             }
             close = true;
         }
     }
-
-//    class ServerThread extends Thread {
-//        private ServerSocket serverSocket;
-//        public ServerThread(ServerSocket serverSocket){
-//            this.serverSocket=serverSocket;
-//        }
-//        public void run() {
-//            while (true){
-//                try {
-//                    Socket socket = serverSocket.accept();
-//                    IO client = new IO(socket);
-//                    client.start();// 开启对此客户端服务的线程
-//                    clients.add(client);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//    }
-
-
-    
-//    public class IO extends Thread{
-//        private Socket socket;
-//        private PrintWriter writer = null;
-//        private BufferedReader reader = null;
-//        private boolean close = false;
-//
-//        public PrintWriter getWriter() {
-//            return writer;
-//        }
-//
-//        public IO(Socket socket){
-//            try {
-//                this.socket=socket;
-//                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        public void run() {
-//            Start();
-//        }
-//        public void InPut() {
-//            try {
-//                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//                String string = reader.readLine();
-//                System.out.println(socket.getRemoteSocketAddress() + " 发来 ：" + string);
-//                OutPut(string);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        public void OutPut(String string) {
-//            try {
-//                writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-////            String str = scanner.next();
-//
-//                writer.println(string);
-//                writer.flush();
-//                if (string.equals("exit")) {
-//                    Close();
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        public void Start() {
-//            try {
-//                writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-//                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-////            System.out.println(socket);
-////            System.out.println(socket.getRemoteSocketAddress());
-//                System.out.println("addr：" + socket.getInetAddress() + " ；port: " + socket.getPort() + " 已连接");
-//
-//            } catch (IOException e) {
-//                System.out.println("客户端连接失败");
-//                e.printStackTrace();
-//            }
-//
-//        }
-//
-//        public void Close() {
-//            try {
-////            serverSocket.close();
-//                socket.close();
-//                writer.close();
-//                reader.close();
-//                System.out.println("addr：" + socket.getInetAddress() + " ；port: " + socket.getPort() + " 已退出");
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                System.out.println("关闭失败");
-//            }
-//            close = true;
-//        }
-//    }
 }
-
