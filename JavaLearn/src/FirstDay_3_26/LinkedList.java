e349c12da870ab6eaf3b383ea06cf102aa9aa1e2
necromaner <necromaner@foxmail.com>
2018-03-27 13:04:34 +0800

    3.27

---
+++ JavaLearn/src/FirstDay_3_26/LinkedList.java
@@ -0,0 +1,186 @@
+package FirstDay_3_26;
+
+/**
+ * java类简单作用描述
+ * Description:
+ * User: necromaner
+ * Date: 2018-03-27
+ *
+ * @Description: java类作用描述
+ * Time: 下午12:17
+ */
+public class LinkedList<T> {
+    
+    /**
+     * class node
+     * @author egg
+     * @param <T>
+     */
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        
+        Node(T data, Node<T> next) {
+            this.data = data;
+            this.next = next;
+        }
+        
+        Node(T data) {
+            this(data, null);
+        }
+    }
+    
+    // data
+    private Node<T> head, tail;
+    
+    public LinkedList() {
+        head = tail = null;
+    }
+    
+    /**
+     * judge the list is empty
+     */
+    public boolean isEmpty() {//判断是否为空，空->ture
+        return head == null;
+    }
+    
+    /**
+     * add head node
+     */
+    public void addHead(T item) {//添加头节点
+        head = new Node<T>(item);
+        if (tail == null)
+            tail = head;
+    }
+    
+    /**
+     * add the tail pointer
+     */
+    public void addTail(T item) {//添加尾指针,有头后面加，没头加头
+        if (!isEmpty()) {
+            tail.next = new Node<T>(item);
+            tail = tail.next;
+        } else {
+            head = tail = new Node<T>(item);
+        }
+    }
+    /**
+     * print the list
+     */
+    public void traverse() {//打印
+        if (isEmpty()) {
+            System.out.println("null");
+        } else {
+//            for (Node<T> p = head; p != null; p = p.next)
+//                System.out.println(p.data);
+            for (Node<T> p = head; p != null; p = p.next){
+                System.out.print(p.data);
+                if (p.next!=null)
+                    System.out.print("-");
+            }
+            System.out.println("");
+        }
+    }
+    
+    /**
+     * insert node from head
+     */
+    public void addFromHead(T item) {//头前插入
+        Node<T> newNode = new Node<T>(item);
+        newNode.next = head;
+        head = newNode;
+    }
+    
+    /**
+     * insert node from tail
+     */
+    public void addFromTail(T item) {//尾后插入
+        Node<T> newNode = new Node<T>(item);
+        Node<T> p = head;
+        while (p.next != null)
+            p = p.next;
+        p.next = newNode;
+        newNode.next = null;//释放内存
+    }
+    
+    /**
+     * delete node from head
+     */
+    public void removeFromHead() {//删除头
+        if (!isEmpty())
+            head = head.next;
+        else
+            System.out.println("The list have been emptied!");
+    }
+    
+    /**
+     * delete frem tail, lower effect
+     */
+    public void removeFromTail() {//删除尾部
+        Node<T> prev = null, curr = head;
+        while (curr.next != null) {
+            prev = curr;
+            curr = curr.next;
+            if (curr.next == null)
+                prev.next = null;
+        }
+    }
+    
+    /**
+     * insert a new node
+     * @param appointedItem
+     * @param item
+     * @return
+     */
+    public boolean insert(T appointedItem, T item) {//插入(位置，内容）
+        Node<T> prev = head, curr = head.next, newNode;
+        newNode = new Node<T>(item);
+        if (!isEmpty()) {
+            while ((curr != null) && (!appointedItem.equals(curr.data))) {
+                prev = curr;
+                curr = curr.next;
+            }
+            newNode.next = curr;
+            prev.next = newNode;
+            return true;
+        }
+        return false;
+    }
+    
+    public void remove(T item) {
+        Node<T> curr = head, prev = null;
+        boolean found = false;
+        while (curr != null && !found) {
+            if (item.equals(curr.data)) {
+                if (prev == null)
+                    removeFromHead();
+                else
+                    prev.next = curr.next;
+                found = true;
+            } else {
+                prev = curr;
+                curr = curr.next;
+            }
+        }
+    }
+    
+    
+    public int indexOf(T item) {
+        int index = 0;
+        Node<T> p;
+        for (p = head; p != null; p = p.next) {
+            if (item.equals(p.data))
+                return index;
+            index++;
+            
+        }
+        return -1;
+    }
+    
+    /**
+     * judge the list contains one data
+     */
+    public boolean contains(T item) {
+        return indexOf(item) != -1;
+    }
+}
---
+++ JavaLearn/src/FirstDay_3_26/T8_DataStructure.java
@@ -0,0 +1,146 @@
+package FirstDay_3_26;
+
+/**
+ * java类简单作用描述
+ * Description:
+ * User: necromaner
+ * Date: 2018-03-27
+ *
+ * @Description: java类作用描述
+ * Time: 上午11:37
+ */
+/*
+https://blog.csdn.net/zhangerqing/article/details/8796518
+ */
+public class T8_DataStructure {
+    public static void main(String[] args) {
+        System.out.println("基础数据结构:");
+        int a[];//栈
+        int[] b;
+//        int []c;
+        a = new int[10];//堆
+        int c[] = {2, 3, 6, 10, 99};
+        int[] d = new int[10];
+        int e[] = new int[10];
+        System.out.println("数组:");
+//        我们可以通过下标来检索数组。下面我举个简单的例子，来说明下数组的用法。
+        String name[];
+        name = new String[5];
+        name[0] = "egg";
+        name[1] = "erqing";
+        name[2] = "baby";
+        for (int i = 0; i < name.length; i++) {
+            if (name[i] != null)
+                System.out.print(name[i]);
+            else break;
+            if (i < name.length - 1)
+                if (name[i + 1] != null)
+                    System.out.print("---");
+        }
+        System.out.println("");
+        for (String i:name) {
+            System.out.print(i+"   ");
+        }
+        System.out.println("");
+//        这是最简单的数组声明、创建、赋值、遍历的例子，下面写个增删的例子。
+        int value[] = new int[10];
+        for (int i = 0; i < 10; i++) {
+            value[i] = i;
+        }
+    
+        // traverse(value);
+        // insert(value, 666, 5);
+        traverse(value);
+        value=delete(value, 2);
+        traverse(value);
+        value=delete(value, 2,3);
+        traverse(value);
+        value=insert(value,5,3);
+        traverse(value);
+    
+        System.out.println("单链表:");
+        LinkedList linkedList=new LinkedList();
+        linkedList.addHead(1);
+        linkedList.addTail(2);
+        linkedList.addTail(3);
+        linkedList.addTail(4);
+        linkedList.addTail(9);
+        linkedList.addTail(8);
+        linkedList.traverse();
+        linkedList.addFromHead(10);
+        linkedList.traverse();
+        linkedList.addFromTail(20);
+        linkedList.traverse();
+        linkedList.removeFromHead();
+        linkedList.traverse();
+        linkedList.removeFromTail();
+        linkedList.traverse();
+        linkedList.insert(3,100);
+    
+        linkedList.traverse();
+    }
+    public static int[] insert(int[] old,int value,int index){//插入
+        int[] n=new int[old.length+1];
+        for (int i = 0; i < n.length; i++) {
+            if (i<index)
+                n[i]=old[i];
+            else if (i==index)
+                n[i]=value;
+            else
+                n[i]=old[i-1];
+        }
+        return n;
+    }
+//    public static int[] insert(int[] old, int value, int index) {
+//        for (int k = old.length - 1; k > index; k--)
+//            old[k] = old[k - 1];
+//        old[index] = value;
+//        return old;
+//    }
+
+
+    public static void traverse(int data[]) {//输出
+        for (int j = 0; j < data.length; j++) {
+            System.out.print(data[j]);
+            if (j<data.length-1)
+                System.out.print("-");
+        }
+        System.out.println();
+        
+    }
+////    public static void traverse(int data[]) {
+//        for (int j = 0; j < data.length; j++)
+//            System.out.print(data[j] + " ");
+//        System.out.println();
+//    }
+    
+    public static int[] delete(int[] old, int index) {//删除一个元素
+        int[] n=new int[old.length-1];
+        for (int i = 0; i < n.length; i++) {
+            if (i<index)
+                n[i]=old[i];
+            else
+                n[i]=old[i+1];
+        }
+        return n;
+    }
+    public static int[] delete(int[] old, int index,int l) {//删除多个元素
+        if (l<1)
+            return old;
+        int[] n=new int[old.length-l];
+        for (int i = 0; i < n.length; i++) {
+            if (i<index)
+                n[i]=old[i];
+            else
+                n[i]=old[i+l];
+        }
+        return n;
+    }
+//    public static int[] delete(int[] old, int index) {
+//        for (int h = index; h < old.length - 1; h++) {
+//            old[h] = old[h + 1];
+//        }
+//        old[old.length - 1] = 0;
+//        return old;
+//    }
+}
